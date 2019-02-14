e2e98ba034d3a765bacac32338248fe72e462e0b
Jeremy Turinetti <jturinetti@users.noreply.github.com>
2019-02-14 15:17:26 -0600

    renamed files, functions, and objects

--- lambdas/iot-rememberwhen/Handler.cs
+++ lambdas/iot-rememberwhen/Handler.cs
@@ -1,5 +1,5 @@
-using Amazon.Lambda.Core;
 using System;
+using Amazon.Lambda.Core;
 
 [assembly:LambdaSerializer(typeof(Amazon.Lambda.Serialization.Json.JsonSerializer))]
 
@@ -7,33 +7,19 @@ namespace AwsDotnetCsharp
 {
     public class Handler
     {
-       public Response Hello(Request request)
+       public Response Reminisce()
        {
-           return new Response("Go Serverless v1.0! Your function executed successfully!", request);
+           return new Response("Go Serverless v1.0! Your function executed successfully!");
        }
     }
 
     public class Response
     {
       public string Message {get; set;}
-      public Request Request {get; set;}
 
-      public Response(string message, Request request){
+      public Response(string message)
+      {
         Message = message;
-        Request = request;
-      }
-    }
-
-    public class Request
-    {
-      public string Key1 {get; set;}
-      public string Key2 {get; set;}
-      public string Key3 {get; set;}
-
-      public Request(string key1, string key2, string key3){
-        Key1 = key1;
-        Key2 = key2;
-        Key3 = key3;
       }
     }
 }
