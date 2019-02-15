a962da63525b527a4234fd557749865f14f28424
Jeremy Turinetti <jturinetti@users.noreply.github.com>
2019-02-15 10:24:16 -0600

    normalized line endings and tabs/spaces

--- lambdas/iot-rememberwhen/Handler.cs
+++ lambdas/iot-rememberwhen/Handler.cs
@@ -14,8 +14,8 @@ namespace AwsDotnetCsharp
     {
         const string MyEmail = "jeremy.turinetti@gmail.com";
 
-       public async Task<Response> Reminisce()
-       {
+        public async Task<Response> Reminisce()
+        {
             var memoryToSend = SelectMemory();
 
             using (var ses = new AmazonSimpleEmailServiceClient(RegionEndpoint.USWest2))
@@ -35,31 +35,31 @@ namespace AwsDotnetCsharp
             }
 
             return new Response(memoryToSend);
-       }
+        }
 
-       private string SelectMemory()
-       {
-         var memories = new List<string>
-         {
-           "Remember that time we got married?",
-           "Remember that time we took a trip to Europe to see Italy and Greece?",
-           "Remember the night we met at Kara's party?"
-         };
+        private string SelectMemory()
+        {
+            var memories = new List<string>
+            {
+            "Remember that time we got married?",
+            "Remember that time we took a trip to Europe to see Italy and Greece?",
+            "Remember the night we met at Kara's party?"
+            };
 
-         var randomizer = new Random(DateTime.UtcNow.DayOfYear + DateTime.UtcNow.Second); // this could be better
-         var randomIndex = randomizer.Next(memories.Count);
+            var randomizer = new Random(DateTime.UtcNow.DayOfYear + DateTime.UtcNow.Second); // this could be better
+            var randomIndex = randomizer.Next(memories.Count);
 
-         return memories[randomIndex];
-       }
+            return memories[randomIndex];
+        }
     }
 
     public class Response
     {
-      public string Message {get; set;}
+        public string Message {get; set;}
 
-      public Response(string message)
-      {
-        Message = message;
-      }
+        public Response(string message)
+        {
+            Message = message;
+        }
     }
 }
