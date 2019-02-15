b0b44b51a9a3a2a0ef53c9645e830a145ab9f63e
Jeremy Turinetti <jturinetti@users.noreply.github.com>
2019-02-15 11:00:50 -0600

    add Twilio functionality; pull variables from environment vars

--- lambdas/iot-rememberwhen/Handler.cs
+++ lambdas/iot-rememberwhen/Handler.cs
@@ -5,6 +5,8 @@ using Amazon;
 using Amazon.Lambda.Core;
 using Amazon.SimpleEmail;
 using Amazon.SimpleEmail.Model;
+using Twilio;
+using Twilio.Rest.Api.V2010.Account;
 
 [assembly: LambdaSerializer(typeof(Amazon.Lambda.Serialization.Json.JsonSerializer))]
 
@@ -12,7 +14,14 @@ namespace AwsDotnetCsharp
 {
     public class Handler
     {
-        const string MyEmail = "jeremy.turinetti@gmail.com";
+        string _wifeEmail = "";
+        string _wifePhone = "";
+        string _husbandPhone = Environment.GetEnvironmentVariable("HusbandPhoneNumber");
+        string _husbandEmail = Environment.GetEnvironmentVariable("HusbandEmail");
+
+        string _twilioAccountSid = Environment.GetEnvironmentVariable("TwilioAccountSid");
+        string _twilioAuthToken = Environment.GetEnvironmentVariable("TwilioAuthToken");
+        string _twilioPhoneNumber = Environment.GetEnvironmentVariable("TwilioPhoneNumber");
 
         public async Task<Response> Reminisce()
         {
@@ -22,16 +31,26 @@ namespace AwsDotnetCsharp
             {
                 // TODO: verify email identities here
 
+                // send email(s)
                 await ses.SendEmailAsync(new SendEmailRequest
                 {
-                    Source = MyEmail,
-                    Destination = new Destination(new List<string> {  MyEmail }),
+                    Source = _husbandEmail,
+                    Destination = new Destination(new List<string> { _husbandEmail }),
                     Message = new Message
                     {
                         Body = new Body(new Content(memoryToSend)),
-                        Subject = new Content("Thinking of you")
+                        Subject = new Content("thinking of you")
                     }
                 });
+
+                // send text(s)
+                TwilioClient.Init(_twilioAccountSid, _twilioAuthToken);
+
+                var textMessage = MessageResource.Create(
+                    body: memoryToSend,
+                    from: new Twilio.Types.PhoneNumber(_twilioPhoneNumber),
+                    to: new Twilio.Types.PhoneNumber(_husbandPhone)
+                );
             }
 
             return new Response(memoryToSend);
@@ -39,11 +58,12 @@ namespace AwsDotnetCsharp
 
         private string SelectMemory()
         {
+
             var memories = new List<string>
             {
-            "Remember that time we got married?",
-            "Remember that time we took a trip to Europe to see Italy and Greece?",
-            "Remember the night we met at Kara's party?"
+                "Remember that time we got married?",
+                "Remember that time we took a trip to Europe to see Italy and Greece?",
+                "Remember the night we met at Kara's party?"
             };
 
             var randomizer = new Random(DateTime.UtcNow.DayOfYear + DateTime.UtcNow.Second); // this could be better
