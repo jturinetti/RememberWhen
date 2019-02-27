c7eee633fb8163c68a0dc5c89e6d7b1233d90d3e
Jeremy Turinetti <jturinetti@users.noreply.github.com>
2019-02-26 20:45:48 -0600

    add service class and interface stubs #11

---
+++ lambdas/RememberWhen.Lambda.Services/EmailService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RememberWhen.Lambda.Services
+{
+    public interface IEmailService
+    {
+
+    }
+
+    public class EmailService : IEmailService
+    {
+
+    }
+}
---
+++ lambdas/RememberWhen.Lambda.Services/ParameterManagementService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RememberWhen.Lambda.Services
+{
+    public interface IParameterManagementService
+    {
+
+    }
+
+    public class ParameterManagementService : IParameterManagementService
+    {
+
+    }
+}
