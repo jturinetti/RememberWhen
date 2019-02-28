d7d492cee44a4da55e60b719f27dbb5ffd0c029d
Jeremy Turinetti <jturinetti@users.noreply.github.com>
2019-02-28 11:05:53 -0600

    wire up application service to handler

--- lambdas/RememberWhen.Lambda/Handler.cs
+++ lambdas/RememberWhen.Lambda/Handler.cs
@@ -1,31 +1,20 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
-using Amazon;
 using Amazon.Lambda.Core;
-using Amazon.SimpleEmail;
-using Amazon.SimpleEmail.Model;
-using Amazon.SimpleSystemsManagement;
-using Amazon.SimpleSystemsManagement.Model;
 using Microsoft.Extensions.DependencyInjection;
 using RememberWhen.Lambda.Models;
 using RememberWhen.Lambda.Services;
-using Twilio;
-using Twilio.Rest.Api.V2010.Account;
 
 [assembly: LambdaSerializer(typeof(Amazon.Lambda.Serialization.Json.JsonSerializer))]
 namespace RememberWhen.Lambda
 {
     public class Handler
     {
-        private IDictionary<string, string> _parameterDictionary;
-        private bool _isProduction;
-
-        private IApplicationService _application;
+        private readonly IApplicationService _application;
 
         public Handler()
         {
+            var serviceProvider = InitializeApplication();
+            _application = serviceProvider.GetService<IApplicationService>();
         }
 
         private ServiceProvider InitializeApplication()
@@ -44,47 +33,14 @@ namespace RememberWhen.Lambda
             return serviceProvider;
         }
 
+        /// <summary>
+        /// Lambda handler method called by AWS at runtime.
+        /// </summary>
+        /// <returns></returns>
         public async Task<RememberWhenResponseModel> Reminisce()
         {
-            // TODO: move what we can from here to constructor?
-            var serviceProvider = InitializeApplication();
-            _application = serviceProvider.GetService<IApplicationService>();
-
-            // retrieve all sensitive parameters
-            var parameterService = serviceProvider.GetService<IParameterManagementService>();
-            _parameterDictionary = await parameterService.RetrieveParameters(Constants.ParameterKeys);
-
-            // determine environment
-            var environmentService = serviceProvider.GetService<IEnvironmentManagementService>();
-            _isProduction = environmentService.EnvironmentType == Environments.Production;
-
-            // select memory
-            var memoryService = serviceProvider.GetService<IMemoryService>();
-            var memoryToSend = memoryService.RetrieveRandomMemory();
-
-            // find email addresses to send memory to
-            var emailsToSendTo = new List<string> { _parameterDictionary[Constants.HusbandEmailKey] };
-            if (_isProduction)
-            {
-                emailsToSendTo.Add(_parameterDictionary[Constants.WifeEmailKey]);
-            }
-
-            // send memory to email addresses
-            var emailService = serviceProvider.GetService<IEmailService>();
-            await emailService.SendMemory(memoryToSend, _parameterDictionary[Constants.HusbandEmailKey], emailsToSendTo);
-
-            if (_isProduction)
-            {
-                // send memory to phones via text
-                var textMessageService = serviceProvider.GetService<ITextMessageService>();
-                await textMessageService.SendMemory(
-                    memoryToSend,
-                    _parameterDictionary[Constants.TwilioPhoneNumberKey],
-                    new string[2] { _parameterDictionary[Constants.HusbandPhoneNumberKey], _parameterDictionary[Constants.WifePhoneNumberKey] }
-                );
-            }
-
-            return new RememberWhenResponseModel(memoryToSend);
+            var result = await _application.Run();
+            return result;
         }
     }
 }
