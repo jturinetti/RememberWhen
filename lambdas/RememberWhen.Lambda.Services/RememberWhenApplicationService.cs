290727a7d1fa2861ef2ca42f5219eb328884e347
Jeremy Turinetti <jturinetti@users.noreply.github.com>
2019-02-28 11:02:24 -0600

    flesh out application service to handle app control flow

--- lambdas/RememberWhen.Lambda.Services/RememberWhenApplicationService.cs
+++ lambdas/RememberWhen.Lambda.Services/RememberWhenApplicationService.cs
@@ -1,37 +1,68 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
 using RememberWhen.Lambda.Models;
 
 namespace RememberWhen.Lambda.Services
 {
     public interface IApplicationService
     {
-        RememberWhenResponseModel Run();
+        Task<RememberWhenResponseModel> Run();
     }
 
     public class RememberWhenApplicationService : IApplicationService
     {
-        private readonly IMemoryService _memoryService;
+        private readonly IEnvironmentManagementService _environmentService;
         private readonly IParameterManagementService _parameterManagementService;
+        private readonly IMemoryService _memoryService;
         private readonly IEmailService _emailService;
         private readonly ITextMessageService _textMessageService;
 
-        public RememberWhenApplicationService(
+        public RememberWhenApplicationService(            
+            IParameterManagementService parameterManagementService,
+            IEnvironmentManagementService environmentService,
             IMemoryService memoryService,
-            IParameterManagementService parameterManagementService, 
             IEmailService emailService, 
             ITextMessageService textMessageService)
         {
-            _memoryService = memoryService;
+            _environmentService = environmentService;
             _parameterManagementService = parameterManagementService;
+            _memoryService = memoryService;
             _emailService = emailService;
             _textMessageService = textMessageService;
         }
 
-        public RememberWhenResponseModel Run()
+        public async Task<RememberWhenResponseModel> Run()
         {
-            throw new NotImplementedException();
+            // retrieve all sensitive parameters
+            var parameterDictionary = await _parameterManagementService.RetrieveParameters(Constants.ParameterKeys);
+
+            // determine environment
+            var isProduction = _environmentService.EnvironmentType == Environments.Production;
+
+            // select memory
+            var memoryToSend = _memoryService.RetrieveRandomMemory();
+
+            // find email addresses to send memory to
+            var emailsToSendTo = new List<string> { parameterDictionary[Constants.HusbandEmailKey] };
+            if (isProduction)
+            {
+                emailsToSendTo.Add(parameterDictionary[Constants.WifeEmailKey]);
+            }
+
+            // send memory to email addresses
+            await _emailService.SendMemory(memoryToSend, parameterDictionary[Constants.HusbandEmailKey], emailsToSendTo);
+
+            if (isProduction)
+            {
+                // send memory to phones via text
+                await _textMessageService.SendMemory(
+                    memoryToSend,
+                    parameterDictionary[Constants.TwilioPhoneNumberKey],
+                    new string[2] { parameterDictionary[Constants.HusbandPhoneNumberKey], parameterDictionary[Constants.WifePhoneNumberKey] }
+                );
+            }
+
+            return new RememberWhenResponseModel(memoryToSend);
         }
     }
 }
