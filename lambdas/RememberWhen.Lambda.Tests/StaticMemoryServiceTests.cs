88f7faf9205b134dd67811d263e3ab976c302e5a
Jeremy Turinetti <jturinetti@users.noreply.github.com>
2019-03-10 16:25:12 -0500

    simple static memory test addition

---
+++ lambdas/RememberWhen.Lambda.Tests/StaticMemoryServiceTests.cs
@@ -0,0 +1,23 @@
+﻿using RememberWhen.Lambda.Services;
+using Xunit;
+
+namespace RememberWhen.Lambda.Tests
+{
+    public class StaticMemoryServiceTests
+    {
+        private readonly IMemoryService _sut;
+
+        public StaticMemoryServiceTests()
+        {
+            _sut = new StaticMemoryService();
+        }
+
+        [Fact]
+        public void RetrieveRandomMemory_ReturnsPopulatedMemoryString()
+        {
+            var result = _sut.RetrieveRandomMemory();
+
+            Assert.False(string.IsNullOrEmpty(result));
+        }
+    }
+}
